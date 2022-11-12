23f9507507e41f493e96a6e2cee09c58ad0ec737
gapatronh <gapatronh@gmail.com>
2022-11-11 22:02:53 -0500

    Begin writing down structure for main text reader classes.

---
+++ backend/sentiment.py
@@ -0,0 +1,23 @@
+import spacy 
+
+# Apparently the avg silent reading adult reads between 238 words per minute (wpm) and 260 wpm. 
+# source: https://psyarxiv.com/xynwg/
+
+class Reader:
+    """
+    Standard Reader class, has functions for text reading and preprocessing
+    """
+    pass
+
+
+class ReaderImage(Reader):
+    """
+    
+    """
+    pass
+
+class ReaderAudio(Reader):
+    """
+    
+    """
+    pass
